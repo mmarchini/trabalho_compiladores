819017dc62fe0fe24b8dd1b5a81e4717fc746fde
Matheus Marchini <matheus@sthima.com.br>
2014-03-12 14:27:16 -0300

    atualizando o script de compactação e sincronização

--- scripts/etapa1.sh
+++ scripts/etapa1.sh
@@ -1,24 +1,36 @@
+# Cria pasta tmp
 if [ -d "tmp" ]; then
     rm -rf tmp;
 fi
 mkdir tmp;
-cp Makefile tmp/;
-cp analisador.l tmp/;
-cp main.c tmp/;
 
+# Copia os arquivos para a pasta tmp e entra na pasta tmp
+for file in "hash.c hash.h main.c Makefile scanner.l tokens.h util.c util.h"
+do
+    cp $file tmp/;
+    cp $file tmp/;
+    cp $file tmp/;
+done;
 cd tmp;
-tar cvzf etapa1.tgz .;
-mv etapa1.tgz ../;
+
+# Compacta os arquivos da pasta tmp e move ele para a raiz
+tar cvzf .temporary_file.tgz .;
+mv .temporary_file.tgz ../etapa1.tgz ;
+
+# Volta para a raiz e move o arquivo para a pasta etapas
 cd ..;
 if [ ! -d "etapas" ]; then
     mkdir etapas;
 fi
-if [ ! -f "etapas/etapa1.tgz" ]; then
-    rm etapas/etapa1.tgz;
+if [ -f "etapas/etapa1.tgz" ]; then
+    rm -f etapas/etapa1.tgz;
 fi
 mv etapa1.tgz etapas;
+
+# Remove a pasta tmp
 rm -rf tmp;
 
+# Tenta sincronizar o arquivo com o servidor
 if [ ! -z "$1" ]; then
     rsync etapas/etapa1.tgz $1@html.inf.ufrgs.br:/home/grad/$1/public_html/etapa1.tgz;
 fi
