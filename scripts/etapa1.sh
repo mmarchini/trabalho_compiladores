9d828a4edaa9f880d7e2193955800c4b2c3f5eb5
Matheus Marchini <matheusdot@gmail.com>
2014-04-02 20:20:18 -0300

    arrumando uns detalhes

--- scripts/etapa1.sh
+++ scripts/etapa1.sh
@@ -5,7 +5,7 @@ fi
 mkdir tmp;
 
 # Copia os arquivos para a pasta tmp e entra na pasta tmp
-for file in "hash.c hash.h main.c Makefile scanner.l tokens.h util.c util.h"
+for file in "hash.c hash.h main.c Makefile scanner.l syntax.y symbols.h util.c util.h"
 do
     cp $file tmp/;
     cp $file tmp/;
@@ -15,24 +15,24 @@ cd tmp;
 
 # Compacta os arquivos da pasta tmp e move ele para a raiz
 tar cvzf .temporary_file.tgz .;
-mv .temporary_file.tgz ../etapa1.tgz ;
+mv .temporary_file.tgz ../etapa2.tgz ;
 
 # Volta para a raiz e move o arquivo para a pasta etapas
 cd ..;
 if [ ! -d "etapas" ]; then
     mkdir etapas;
 fi
-if [ -f "etapas/etapa1.tgz" ]; then
-    rm -f etapas/etapa1.tgz;
+if [ -f "etapas/etapa2.tgz" ]; then
+    rm -f etapas/etapa2.tgz;
 fi
-mv etapa1.tgz etapas;
+mv etapa2.tgz etapas;
 
 # Remove a pasta tmp
 rm -rf tmp;
 
 # Tenta sincronizar o arquivo com o servidor
 if [ ! -z "$1" ]; then
-    rsync etapas/etapa1.tgz $1@html.inf.ufrgs.br:/home/grad/$1/public_html/etapa1.tgz;
+    rsync etapas/etapa2.tgz $1@html.inf.ufrgs.br:/home/grad/$1/public_html/etapa2.tgz;
 fi
 
 
